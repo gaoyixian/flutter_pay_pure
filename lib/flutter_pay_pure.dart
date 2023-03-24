c1d47ebca556e3b3fa233e05f1f5a4c7ed0cc0dc
fangwy <fangwy2@gmail.com>
2023-03-24 14:07:41 +0800

    接口

--- lib/flutter_pay_pure.dart
+++ lib/flutter_pay_pure.dart
@@ -1,23 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_pay_interface/flutter_pay_interface.dart';
 import 'package:flutter_pay_ios/flutter_pay.dart';
 import 'package:flutter_pay_android/flutter_pay.dart';
 import 'package:universal_platform/universal_platform.dart';
-export 'package:flutter_pay_android/android_widgets.dart';
 
 class FlutterPayPure {
   static late FlutterPayInterface _interface;
+  static FlutterPayInterface get interface => _interface;
   static Future<void> init(
       {required VerifyReceipt verifyReceipt,
       required LocalizationText localizationText,
-      required void Function() onError}) async {
+      required void Function() onError,
+      required ShowBottomSheet showBottomSheet,
+      required withDrawalMgr}) async {
     if (UniversalPlatform.isIOS) {
       _interface = FlutterPayIos();
     } else if (UniversalPlatform.isAndroid) {
-      _interface = FlutterPayAndroid();
+      _interface = FlutterPayAndroid(withDrawalMgr);
     }
     _interface.init(
         verifyReceipt: verifyReceipt,
         localizationText: localizationText,
+        showBottomSheet: showBottomSheet,
         onError: onError);
   }
 
@@ -26,11 +31,31 @@ class FlutterPayPure {
     _interface.pay(rsp, time);
   }
 
+  // 获取支付按钮
+  static Widget getPlayButton(BuildContext context, double rate, int chooseIndex, void Function (int index, int typ) toPay){
+    return _interface.getPlayButton(context, rate, chooseIndex, toPay);
+  }
+  static void paymethodBottom(BuildContext context,
+      {required int id,
+      required int gold,
+      required int rmb,
+      required void Function(int p1, int p2) toPay}) {
+    _interface.paymethodBottom(context,
+        id: id, gold: gold, rmb: rmb, toPay: toPay);
+  }
+
   static Future<void> restorePurchases() async {
     await _interface.restorePurchases();
   }
 
   static Future<void> logout() async {
     await _interface.logout();
+  }
+
+  
+
+  static Future<void> showBottomSheet(
+      {required BuildContext context,
+      required Widget container}) async {
   }
 }
